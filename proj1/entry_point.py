36c3e56cf742a64d7ebd179dbd563e1abf9c69ab
Tamil <tamil@tamils.rocks>
2023-01-18 21:53:46 -0500

    python hands-on

---
+++ proj1/entry_point.py
@@ -0,0 +1,12 @@
+import sys
+# from fruits.apple import *
+import outsider
+from fruits.apple import Apple
+
+a = Apple("hawaii")
+print(a.display())
+print(a.meta_test())
+
+print(sys.path)
+# outsider.display()
+print(outsider.display())
